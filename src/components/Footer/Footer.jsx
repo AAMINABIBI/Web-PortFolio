4e5ee1b5c4e06bb8a13b3a3e692a441d283e4361
Aamina Bibi <aaminabibi2324@gmail.com>
2023-08-16 10:29:54 +0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import './footer.css'
+import './Footer.css';
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-}
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="footer-content">
+                    <p>&copy; 2023 <span> Aamina Bibi.</span> All rights reserved.</p>
+                   
+                </div>
+            </div>
+        </footer>
+    );
+};
 
 export default Footer;
